fac4e33cdd3de86735e9ac979de1a90abc7286d3
Fabio jose <zthiner3@gmail.com>
2026-07-12 22:11:38 -0300

    adicionando testes da API

--- src/main/java/com/example/demo/service/ManutencaoService.java
+++ src/main/java/com/example/demo/service/ManutencaoService.java
@@ -7,7 +7,7 @@ import com.example.demo.model.Manutencao;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
-import java.util.Optional;
+//import java.util.Optional;
 
 @Service
 public class ManutencaoService {
